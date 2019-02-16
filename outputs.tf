4f868d637650f847f2527f03a6b78d7a03f98457
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-02-15 22:38:15 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
-
+locals {
+  this_id                           = "${compact(concat(coalescelist(aws_instance.this.*.id, list("")))}"}
+  #this_id                           = "${compact(concat(coalescelist(aws_instance.this.*.id, aws_instance.this_t2.*.id), list("")))}"}
+  output "id" {
+  description = "List of IDs of instances"
+  value       = ["${local.this_id}"]
+}
